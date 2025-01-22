cc1f6311da6e5989bad5e1b32aa5a16dfdbfab7f
Muratcan YÜCE <muratcanyuce@outlook.com>
2025-01-22 17:36:44 +0300

    - Teacher interface service yazıldı. - TeacherManager eklendi.

---
+++ Acme.DersTakip.Business/Abstract/ITeacherService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.DersTakip.Entity.Concrete;
+
+namespace Acme.DersTakip.Business.Abstract
+{
+    public interface ITeacherService
+    {
+        void Add(Teacher teacher);
+    }
+}
---
+++ Acme.DersTakip.Business/Concrete/TeacherManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.DersTakip.Business.Abstract;
+using Acme.DersTakip.DataAccess.Abstract;
+using Acme.DersTakip.Entity.Concrete;
+
+namespace Acme.DersTakip.Business.Concrete
+{
+    public class TeacherManager : ITeacherService
+    {
+        private ITeacherDal _teacherDal;
+
+        public TeacherManager(ITeacherDal teacherDal)
+        {
+            _teacherDal = teacherDal;
+        }
+
+        public void Add(Teacher teacher)
+        {
+            _teacherDal.Add(teacher);
+
+        }
+    }
+}
